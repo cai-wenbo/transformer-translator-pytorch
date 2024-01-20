61a1bdfe63f7a9ef85c4a9e87c00eb2ed7d82262
cai-wenbo <508819545@qq.com>
2024-01-20 16:21:17 +0000

    train_script.py

--- train_script.py
+++ train_script.py
@@ -57,13 +57,28 @@ def load_trails(training_config):
             eval_losses = json.load(file)
             file.close()
 
+    train_accuracy = list()
+    if os.path.exists(training_config['train_accuracy_pth']):
+        with open(training_config['train_accuracy_pth'], 'r') as file:
+            train_accuracy = json.load(file)
+            file.close()
+    
+    eval_accuracy = list()
+    if os.path.exists(training_config['eval_accuracy_pth']):
+        with open(training_config['eval_accuracy_pth'], 'r') as file:
+            eval_accuracy = json.load(file)
+            file.close()
 
-    return step_losses, train_losses, eval_losses
+
+    return step_losses, train_losses, eval_losses, train_accuracy, eval_accuracy
 
 
-def train_eval_loop(training_config, model, dataloader_train, dataloader_eval, optimizer, creterian, step_losses, train_losses, eval_losses, device):
+def train_eval_loop(training_config, model, dataloader_train, dataloader_eval, optimizer, creterian, step_losses, train_losses, eval_losses, train_accuracy, eval_accuracy, device):
     for epoch in range(training_config['num_of_epochs']):
         loss_sum_train = 0
+        err            = 0
+        num_tokens     = 0
+
         model.train()
         #  train loop
         for i, batch in enumerate(dataloader_train):
@@ -83,15 +98,20 @@ def train_eval_loop(training_config, model, dataloader_train, dataloader_eval, o
             loss_sum_train += loss_scalar
             step_losses.append(loss_scalar)
 
+            b_predictions = torch.argmax(b_predicted_log_distributions, dim=2)
+            err += (b_predictions != b_text_trg).sum().item()
+            num_tokens += torch.sum(~b_mask_trg).item()
+            print(num_tokens)
+
+
 
             #  b_predicts = torch.argmax(b_outputs, dim=-1)
             #  correct += (b_predicts == b_labels).sum().item()
 
         train_loss = loss_sum_train / len(dataloader_train)
         train_losses.append(train_loss)
-
-
-
+        train_acc = 1 - err / num_tokens
+        train_accuracy.append(train_acc)
 
 
 
@@ -107,18 +127,21 @@ def train_eval_loop(training_config, model, dataloader_train, dataloader_eval, o
 
             with torch.no_grad():
                 b_predicted_log_distributions = model(b_text_src, b_text_trg, b_mask_src, b_mask_trg)
-
                 b_smooth_label = label_smoothing(b_text_trg, training_config["trg_vocab_size"], training_config["num_special_tokens_trg"])
 
-
                 loss = creterian(b_predicted_log_distributions, b_smooth_label)
-
-
                 loss_scalar = loss.item()
                 loss_sum_eval += loss_scalar
 
+                b_predictions = torch.argmax(b_predicted_log_distributions, dim=2)
+                err += (b_predictions != b_text_trg).sum().item()
+                num_tokens += torch.sum(~b_mask_trg).item()
+
+
         eval_loss = loss_sum_eval / len(dataloader_eval)
         eval_losses.append(eval_loss)
+        eval_acc = 1 - err / num_tokens
+        eval_accuracy.append(eval_acc)
 
 
 
@@ -126,7 +149,7 @@ def train_eval_loop(training_config, model, dataloader_train, dataloader_eval, o
 
 
 
-def save_trails(training_config, step_losses, train_losses, eval_losses):
+def save_trails(training_config, step_losses, train_losses, eval_losses, train_accuracy, eval_accuracy):
     with open(training_config['step_losses_pth'], 'w') as file:
         json.dump(step_losses, file)
         file.close()
@@ -137,6 +160,14 @@ def save_trails(training_config, step_losses, train_losses, eval_losses):
     
     with open(training_config['eval_losses_pth'], 'w') as file:
         json.dump(eval_losses, file)
+        file.close()
+
+    with open(training_config['train_accuracy_pth'], 'w') as file:
+        json.dump(train_accuracy, file)
+        file.close()
+    
+    with open(training_config['eval_accuracy_pth'], 'w') as file:
+        json.dump(eval_accuracy, file)
         file.close()
 
 
@@ -153,7 +184,7 @@ def train(training_config):
 
 
     #  load the losses history
-    step_losses, train_losses, eval_losses = load_trails(training_config)
+    step_losses, train_losses, eval_losses, train_accuracy, eval_accuracy = load_trails(training_config)
 
 
 
@@ -216,7 +247,7 @@ def train(training_config):
         print(f"Using {torch.cuda.device_count()} GPUs.")
         model = nn.DataParallel(model)
 
-    train_eval_loop(training_config, model, dataloader_train, dataloader_eval, optimizer, creterian, step_losses, train_losses, eval_losses, device)
+    train_eval_loop(training_config, model, dataloader_train, dataloader_eval, optimizer, creterian, step_losses, train_losses, eval_losses, train_accuracy, eval_accuracy, device)
 
 
         
@@ -228,40 +259,40 @@ def train(training_config):
     torch.save(model.state_dict(), training_config['model_path_dst'])
 
     #  save the loss of the steps
-    save_trails(training_config, step_losses, train_losses, eval_losses)
+    save_trails(training_config, step_losses, train_losses, eval_losses, train_accuracy, eval_accuracy)
 
 
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--num_of_epochs"    , type=int   , help="number of epochs"                                  , default=20)
-    parser.add_argument("--batch_size"       , type=int   , help="batch size"                                        , default=512)
-    parser.add_argument("--learning_rate"    , type=float , help="learning rate"                                     , default=1e-4)
-    parser.add_argument("--weight_decay"     , type=float , help="weight_decay"                                      , default=1e-4)
-    parser.add_argument("--dropout_probability"     , type=float , help="dropout prob"                                      , default=1e-1)
-    parser.add_argument("--src_vocab_size"       , type=int   , help="src vocab size"                                        , default=5000)
-    parser.add_argument("--trg_vocab_size"       , type=int   , help="trg vocab size"                                        , default=5000)
-    parser.add_argument("--model_dimension"    , type=int   , help="model dimmention"                              , default=512)
-    parser.add_argument("--number_of_heads"    , type=int   , help="number of heads"                              , default=8)
-    parser.add_argument("--number_of_layers"    , type=int   , help="number of layers"                              , default=6)
-    parser.add_argument("--num_special_tokens_trg"    , type=int   , help="number of special tokens of target"                              , default=2)
-    parser.add_argument("--num_labels"       , type=int   , help="types of labels"                                   , default=6)
-    parser.add_argument("--num_neg"       , type=int   , help="num of neg"                                   , default=2)
-    parser.add_argument("--sequence_length"  , type=int   , help="sequence_length"                                   , default=128)
-    parser.add_argument("--tokenizer_path_src"   , type=str   , help="the saved tokenizer of src language"                       , default='./data/tokenizer_zh')
-    parser.add_argument("--tokenizer_path_trg"   , type=str   , help="the saved tokenizer of trg language"                       , default='./data/tokenizer_en')
-    parser.add_argument("--data_path_train_src"   , type=str   , help="the training dataset of src language"                       , default='./data/train.zh')
-    parser.add_argument("--data_path_train_trg"   , type=str   , help="the training dataset of trg language"                       , default='./data/train.en')
-    parser.add_argument("--data_path_eval_src"   , type=str   , help="the eval dataset of src language"                       , default='./data/val.zh')
-    parser.add_argument("--data_path_eval_trg"   , type=str   , help="the eval dataset of trg language"                       , default='./data/val.en')
-    parser.add_argument("--model_path_dst"   , type=str   , help="the directory to save model"                       , default='./saved_models/saved_dict.pth')
-    parser.add_argument("--model_path_src"   , type=str   , help="the directory to load model"                       , default='./saved_models/saved_dict.pth')
-    parser.add_argument("--step_losses_pth"  , type=str   , help="the path of the json file that saves step losses"  , default='./trails/step_losses.json')
-    parser.add_argument("--train_losses_pth" , type=str   , help="the path of the json file that saves train losses" , default='./trails/train_losses.json')
-    parser.add_argument("--eval_losses_pth"  , type=str   , help="the path of the json file that saves eval losses"  , default='./trails/eval_losses.json')
-    #  parser.add_argument("--train_accuracy_pth" , type=str   , help="the path of the json file that saves train accuracy" , default='./trails/train_accuracy.json')
-    #  parser.add_argument("--eval_accuracy_pth"  , type=str   , help="the path of the json file that saves eval accuracy"  , default='./trails/eval_accuracy.json')
+    parser.add_argument("--num_of_epochs"          , type=int   , help="number of epochs"                                    , default=20)
+    parser.add_argument("--batch_size"             , type=int   , help="batch size"                                          , default=512)
+    parser.add_argument("--learning_rate"          , type=float , help="learning rate"                                       , default=1e-4)
+    parser.add_argument("--weight_decay"           , type=float , help="weight_decay"                                        , default=1e-4)
+    parser.add_argument("--dropout_probability"    , type=float , help="dropout prob"                                        , default=1e-1)
+    parser.add_argument("--src_vocab_size"         , type=int   , help="src vocab size"                                      , default=5000)
+    parser.add_argument("--trg_vocab_size"         , type=int   , help="trg vocab size"                                      , default=5000)
+    parser.add_argument("--model_dimension"        , type=int   , help="model dimmention"                                    , default=512)
+    parser.add_argument("--number_of_heads"        , type=int   , help="number of heads"                                     , default=8)
+    parser.add_argument("--number_of_layers"       , type=int   , help="number of layers"                                    , default=6)
+    parser.add_argument("--num_special_tokens_trg" , type=int   , help="number of special tokens of target"                  , default=2)
+    parser.add_argument("--num_labels"             , type=int   , help="types of labels"                                     , default=6)
+    parser.add_argument("--num_neg"                , type=int   , help="num of neg"                                          , default=2)
+    parser.add_argument("--sequence_length"        , type=int   , help="sequence_length"                                     , default=128)
+    parser.add_argument("--tokenizer_path_src"     , type=str   , help="the saved tokenizer of src language"                 , default='./data/tokenizer_zh')
+    parser.add_argument("--tokenizer_path_trg"     , type=str   , help="the saved tokenizer of trg language"                 , default='./data/tokenizer_en')
+    parser.add_argument("--data_path_train_src"    , type=str   , help="the training dataset of src language"                , default='./data/train.zh')
+    parser.add_argument("--data_path_train_trg"    , type=str   , help="the training dataset of trg language"                , default='./data/train.en')
+    parser.add_argument("--data_path_eval_src"     , type=str   , help="the eval dataset of src language"                    , default='./data/val.zh')
+    parser.add_argument("--data_path_eval_trg"     , type=str   , help="the eval dataset of trg language"                    , default='./data/val.en')
+    parser.add_argument("--model_path_dst"         , type=str   , help="the directory to save model"                         , default='./saved_models/saved_dict.pth')
+    parser.add_argument("--model_path_src"         , type=str   , help="the directory to load model"                         , default='./saved_models/saved_dict.pth')
+    parser.add_argument("--step_losses_pth"        , type=str   , help="the path of the json file that saves step losses"    , default='./trails/step_losses.json')
+    parser.add_argument("--train_losses_pth"       , type=str   , help="the path of the json file that saves train losses"   , default='./trails/train_losses.json')
+    parser.add_argument("--eval_losses_pth"        , type=str   , help="the path of the json file that saves eval losses"    , default='./trails/eval_losses.json')
+    parser.add_argument("--train_accuracy_pth"     , type=str   , help="the path of the json file that saves train accuracy" , default='./trails/train_accuracy.json')
+    parser.add_argument("--eval_accuracy_pth"      , type=str   , help="the path of the json file that saves eval accuracy"  , default='./trails/eval_accuracy.json')
 
     
     args = parser.parse_args()
@@ -271,5 +302,3 @@ if __name__ == "__main__":
         training_config[arg] = getattr(args, arg)
 
     train(training_config)
-
-
